db2c2edc02eb213f435f2c27a0bded5f83d2bea6
Gurusankar <your-github-gurusankar.2153020@srec.ac.in>
2026-03-05 22:03:07 +0530

    graze and grain

---
+++ frontend/next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "3000",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
